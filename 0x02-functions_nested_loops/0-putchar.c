cf0b21af5c81c57b3f207d393201cce0adcd1161
Minte5 <mintesnotbelete7@gmail.com>
2022-03-15 14:10:38 -0600

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * main - Prints 'Holberton', followed by a new line.
- *
- * Return: Always 0 (Success)
+ * main - entry block
+ * @void: no argument
+ * Return: 0
  **/
 int main(void)
 {
-_putchar(72);
-_putchar(111);
-_putchar(108);
-_putchar(98);
-_putchar(101);
-_putchar(114);
-_putchar(116);
-_putchar(111);
-_putchar(110);
-_putchar('\n');
-return (0);
+	char ermias[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is readme file
+# C - Functions, nested loops
